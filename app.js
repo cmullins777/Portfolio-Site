4d3c2ba87723b87e50f59644872b735ea24347f7
Cindy Mullins <mullins.cindy@gmail.com>
2019-05-13 22:48:21 -0700

    Remove pug comments, add app.js comments, personalize css and document in README

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
+// Require express, set var for json file and projects
 const express = require('express');
 const app = express();
 const data = require('./data.json');
 const projects = data.projects;
 
+// Use pug templates
 app.set('view engine', 'pug');
+// Use express.static
 app.use('/static', express.static('public'));
 
-
-
-//Configure index route to render "Home" page
+// Configure index route to render "Home" page
 app.get('/', (req, res) => {
   res.render('index', {projects});
 });
-//Configure about route to render "About" page
+
+// Configure about route to render "About" page
 app.get('/about', (req, res) => {
   res.render('about');
 });
 
-//Configure projects route to render "Projects" page
+// Configure projects route to render "Projects" page
+// Redirect non-existant project pages back to Home
 app.get('/projects/:id', (req, res) => {
   const {id} = req.params;
   const project = projects[id];
@@ -27,6 +30,7 @@ app.get('/projects/:id', (req, res) => {
   res.render('project', {project});
 });
 
+// 404 error handling
 app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
@@ -34,6 +38,7 @@ app.use((req, res, next) => {
   next(err);
 });
 
+// Error handling
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
@@ -41,6 +46,7 @@ app.use((err, req, res, next) => {
   res.render('error');
 });
 
+// Set app to listen on localhost:3000
 app.listen(3000, () => {
   console.log("running on localhost:3000");
 });
